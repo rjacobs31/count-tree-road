b24fac24fbfd85c5ac6539c08b42090949c0894b
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2018-07-20 16:53:28 +0200

    Add rendering logic to days since counter.

--- src/Widgets.elm
+++ src/Widgets.elm
@@ -1,9 +1,23 @@
 module Widgets exposing(..)
 
+import Date exposing (Date, toTime)
 import Html exposing (Attribute, Html, div, text)
+import Html.Attributes exposing (class)
+import Time exposing (inHours)
 
-daysSinceCounter : List (Attribute msg) -> Html msg
-daysSinceCounter attributes =
-    div attributes
-    [ text "Counter widget"
-    ]
+daysSinceCounter : Date -> Date -> Html msg
+daysSinceCounter eventDate currentDate =
+    div [ class "widget widget--days-since" ]
+        [ text <| toString <| daysSince eventDate currentDate
+        ]
+
+daysSince : Date -> Date -> Int
+daysSince eventDate currentDate =
+    (toTime eventDate) - (toTime currentDate)
+    |> inHours
+    |> floor
+
+type alias DaysSinceCounter =
+    { eventDate : Date
+    , currentDate : Date
+    }
