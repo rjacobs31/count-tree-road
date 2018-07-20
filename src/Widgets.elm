2d8b0f1c9994259bab92e41d061fc8c44319d695
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-07-20 21:39:03 +0200

    Fix calculation of days since.

--- src/Widgets.elm
+++ src/Widgets.elm
@@ -1,21 +1,26 @@
-module Widgets exposing(..)
+module Widgets exposing (..)
 
 import Date exposing (Date, toTime)
 import Html exposing (Attribute, Html, div, text)
 import Html.Attributes exposing (class)
 import Time exposing (inHours)
 
+
 daysSinceCounter : Date -> Date -> Html msg
 daysSinceCounter eventDate currentDate =
     div [ class "widget widget--days-since" ]
-        [ text <| toString <| daysSince eventDate currentDate
+        [ text <| (toString <| daysSince eventDate currentDate) ++ " days"
         ]
+
 
 daysSince : Date -> Date -> Int
 daysSince eventDate currentDate =
-    (toTime eventDate) - (toTime currentDate)
-    |> inHours
-    |> floor
+    (toTime currentDate)
+        - (toTime eventDate)
+        |> inHours
+        |> flip (/) 24
+        |> floor
+
 
 type alias DaysSinceCounter =
     { eventDate : Date
