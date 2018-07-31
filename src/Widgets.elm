23d1a6fbd516ee83af1e6fbb7430e8210a06a82f
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-07-31 18:51:36 +0200

    Add LCD font to number display.

--- src/Widgets.elm
+++ src/Widgets.elm
@@ -1,7 +1,7 @@
 module Widgets exposing (..)
 
 import Date exposing (Date, toTime)
-import Html exposing (Attribute, Html, div, text)
+import Html exposing (Attribute, Html, div, span, text)
 import Html.Attributes exposing (class)
 import Time exposing (Time, inHours)
 
@@ -10,7 +10,9 @@ daysSinceCounter : Maybe Time -> Maybe Time -> Html msg
 daysSinceCounter eventDate currentDate =
     div [ class "card card--days-since" ]
         [ div [ class "card__header" ]
-            [ text <| daysSince eventDate currentDate ++ " days" ]
+            [ span [ class "lcd-display" ] [ text <| daysSince eventDate currentDate ]
+            , text " days"
+            ]
         ]
 
 
