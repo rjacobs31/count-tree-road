0402129d580f8890377e1374a20462de00c5d545
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-08-01 08:55:07 +0200

    Add stub for about page.

---
+++ src/Page/About.elm
@@ -0,0 +1,11 @@
+module About exposing (..)
+
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
+
+
+view : Html msg
+view =
+    div [ class "page" ]
+        [ text "Have a short story about how this page came to be."
+        ]
