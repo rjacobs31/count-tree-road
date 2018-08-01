9ee5007d6c7980e904c18ac2fc4c290515909d96
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-08-01 15:26:39 +0200

    Add navbar stub.

--- src/Page.elm
+++ src/Page.elm
@@ -1,13 +1,19 @@
 module Page exposing (..)
 
-import Html exposing (Html, div, h1, program, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, a, div, h1, nav, program, text)
+import Html.Attributes exposing (class, href)
+import Html.Events exposing (onClick)
+import Route exposing (Route)
 
 
 type ActivePage
     = Other
     | Home
     | About
+
+
+type Msg
+    = Navigation Route
 
 
 frame : Bool -> ActivePage -> Html msg -> Html msg
@@ -23,7 +29,16 @@ viewHeader : Html msg
 viewHeader =
     div [ class "header" ]
         [ h1 [ class "main-title" ] [ text "Count Tree Road" ]
+        , nav [ class "top-nav" ]
+            [ navLink Route.Home "Home"
+            , navLink Route.About "About"
+            ]
         ]
+
+
+navLink : Route -> String -> Html msg
+navLink route label =
+    a [ class "top-nav__nav-link", href "javascript:void(0)" ] [ text label ]
 
 
 viewFooter : Html msg
