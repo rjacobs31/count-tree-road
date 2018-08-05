2f587471c76af661578aa1bcfd556296edc6cb44
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-08-05 23:32:55 +0200

    Add basic styling for About page.

--- src/Page/About.elm
+++ src/Page/About.elm
@@ -1,13 +1,13 @@
 module Page.About exposing (..)
 
-import Html exposing (Html, div, p, text)
+import Html exposing (Html, div, h2, p, text)
 import Html.Attributes exposing (class)
 
 
 view : Html msg
 view =
-    div [ class "card" ]
-        [ div [ class "card__header" ] [ text "HOW THIS APP CAME TO BE" ]
-        , div [ class "card__body" ]
+    div [ class "page" ]
+        [ h2 [ class "page__heading" ] [ text "HOW THIS APP CAME TO BE" ]
+        , div [ class "page__body" ]
             [ p [] [ text "Have a short story about how this page came to be." ] ]
         ]
